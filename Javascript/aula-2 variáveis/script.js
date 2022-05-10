47c8285552580f58be0d7b26a435d785d11ae3e6
Fabianosd <fabiano.souzadutra@gmail.com>
2022-05-10 17:28:10 -0300

    aula de javascript

---
+++ Javascript/aula-2 variáveis/script.js
@@ -0,0 +1,99 @@
+// //tipos primitivos
+
+// //boolean
+// var VouF = false;
+// console.log(typeof(VouF));
+
+// //numero
+// var numeroQualquer = 1;
+// console.log(typeof(numeroQualquer));
+
+// //string
+// var nome = 'fabiano'
+// console.log(typeof(nome));
+
+// //como declarar
+// // var variavel = 'fabiano';
+// // console.log(variavel);
+
+// // let variavel2 = 'fabiano'
+// // console.log(variavel2);
+
+// // const constante = 'Fabiano';
+// // console.log(constante);
+
+// var escopoGlobal = 'global';
+// console.log(escopoGlobal);
+
+// function escopoLocal()
+// {
+//     let escopoLocalInterno = 'Local';
+//     console.log(escopoLocalInterno);
+// }
+
+// escopoLocal();
+
+//atribuição
+// var atribuição = 'fabiano';
+
+// //var comparacao
+// var comparacao = '0' == 0;
+// console.log(comparacao);
+
+// //comparacao identica
+// var comparacaoIdentica = '0' === 0;
+// console.log(comparacaoIdentica);
+
+//adição
+// var adicao = 1 + 1;
+// console.log(adicao);
+
+// //subtração
+// var subtracao = 2 - 1;
+// console.log(subtracao);
+
+// //multiplicação
+// var multiplicacao = 2 * 2;
+// console.log(multiplicacao);
+
+// //divisãoreal
+// var divisaoreal = 6 / 3;
+// console.log(divisaoreal);
+
+// //divisão inteira
+// var divisaointeira = 5 % 2;
+// console.log(divisaointeira);
+
+// //potenciação
+
+// var potenciacao = 2 ** 10;
+// console.log(potenciacao);
+
+// //maior que
+// var maiorQue = 5 > 2;
+// console.log(maiorQue);
+
+// //menor que
+// var menorQue = 5 < 2;
+// console.log(menorQue);
+
+// // maior ou igual a 
+// var maiorOuIgual = 5 >= 2;
+// console.log(maiorOuIgual);
+
+// // menor  ou igual a 
+// var menorOuIgual = 5 <= 2;
+// console.log(menorOuIgual);
+
+// operador e
+var e = true && true;
+console.log(e);
+
+// operador ou
+var ou = true || false;
+console.log(ou);
+
+// operador não
+var nao = ! true;
+console.log(nao);
+
---
+++ Javascript/aula-3 arrays/scrips.js
@@ -0,0 +1,45 @@
+// // o que são vetores ou arrays
+// //como declarar um array
+// let array = ['string', 1, true];
+// console.log(array);
+
+//pode guardar varios tipos de dados
+let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4'],];
+// console.log(array[3]);
+
+//forEach
+//array.forEach(function(item, index){console.log(item, index)});
+
+//push
+// array.push('novo item');
+// console.log(array);
+
+//pop 
+// array.pop();
+// console.log(array);
+
+//shift
+// array.shift();
+// console.log(array);
+
+// //unshift
+// array.unshift('novo item no inicio');
+// console.log(array);
+
+// //indeoff
+// console.log(array.indexOf(true));
+
+// //splice
+// array.splice(0, 3);
+// console.log(array);
+
+// //slice
+
+// let novoArray = array.slice(0, 3);
+// console.log(novoArray);
+
+// let object = { String: 'string', Number: 1, Boolean: true, array:["array"], objectinterno:{objectinterno: 'objeto interno'}};
+// console.log(object.objectinterno);
+
+// var string = Object.String;
+// console.log(string);
