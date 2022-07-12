7dd0d02fa5389a859e15f4fdd9af151fc8fb769b
Fabianosd <fabiano.souzadutra@gmail.com>
2022-07-12 14:16:39 -0300

    Aula 4 de novo

--- Javascript/aula-3 arrays/scrips.js
+++ Javascript/aula-3 arrays/scrips.js
@@ -5,7 +5,7 @@
 
 //pode guardar varios tipos de dados
 let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4'],];
-// console.log(array[3]);
+//console.log(array);
 
 //forEach
 //array.forEach(function(item, index){console.log(item, index)});
